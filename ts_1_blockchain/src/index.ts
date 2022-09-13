7e31587b3e8a0ac91685ab6d0748a5663b8342e5
jhclass <jhcode0@naver.com>
2022-09-13 18:53:14 +0900

    Declaration Files

--- ts_1_blockchain/src/index.ts
+++ ts_1_blockchain/src/index.ts
@@ -1,9 +1,8 @@
-class Block {
-    constructor(
-        private data:string
-    ) {}
-    static hello() {
-        return "hello";
-    }
-}
-//build 전
+import {exit, init} from "myPackage";
+init({
+    url:"true"
+})
+exit(1);
+
+
+localStorage.clear();
