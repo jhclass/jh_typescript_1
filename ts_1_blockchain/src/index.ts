ffa77a7eee64c4789ffd71cba1068fb72af8ab24
jhclass <jhcode0@naver.com>
2022-09-20 17:19:09 +0900

    미니 블록체인 1단계

--- ts_1_blockchain/src/index.ts
+++ ts_1_blockchain/src/index.ts
@@ -21,7 +21,35 @@ class Block implements BlockShape {
     }
     static calculateHash(prevHash:string, height:number, data:string){
         const toHash = `${prevHash}${height}${data}`;
+        return crypto.createHash("sha256").update(toHash).digest("hex");
 
     }
-}   
+}  
+
+class Blockchain {
+    private blocks: Block []
+    constructor(){
+        this.blocks = [];
+    };
+    private getPrevHash() {
+        if (this.blocks.length === 0) return "";
+        return this.blocks[this.blocks.length - 1].hash;
+    }
+    public addBlock(data:string) {
+        const block = new Block(this.getPrevHash(), this.blocks.length+1,data);
+        this.blocks.push(block);
+    }
+    public getBlocks(){
+        return this.blocks;
+    }
+}
+
+const blockchain = new Blockchain();
+blockchain.addBlock("first one");
+blockchain.addBlock("second one");
+blockchain.addBlock("third one");
+
+console.log(blockchain.getBlocks());l //모든 블록체인소환
+
+
 
