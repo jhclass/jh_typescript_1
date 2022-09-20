a204be8663eea7ac76eb9fa493df86b4b1cfd8ef
jhclass <jhcode0@naver.com>
2022-09-20 14:48:30 +0900

    this.hash

--- ts_1_blockchain/build/index.js
+++ ts_1_blockchain/build/index.js
@@ -1,9 +1,16 @@
-var Block = /** @class */ (function () {
-    function Block(data) {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//Block 이 형성안된다.
+//hash가 없어서
+//hash는 prevHash, height , data 값을 이용한 고유한값이다.
+class Block {
+    constructor(prevHash, height, data) {
+        this.prevHash = prevHash;
+        this.height = height;
         this.data = data;
+        this.hash = Block.calculateHash(prevHash, height, data);
     }
-    Block.hello = function () {
-        return "hello";
-    };
-    return Block;
-}());
+    static calculateHash(prevHash, height, data) {
+        const toHash = `${prevHash}${height}${data}`;
+    }
+}
--- ts_1_blockchain/src/index.ts
+++ ts_1_blockchain/src/index.ts
@@ -1,8 +1,27 @@
-import {exit, init} from "myPackage";
-init({
-    url:"true"
-})
-exit(1);
+import crypto from "crypto";
 
+interface BlockShape {
+    hash: string;
+    prevHash:string;
+    height:number;
+    data: string;
+}
+//Block 이 형성안된다.
+//hash가 없어서
+//hash는 prevHash, height , data 값을 이용한 고유한값이다.
 
-localStorage.clear();
+class Block implements BlockShape {
+    public hash : string;
+    constructor(
+        public prevHash:string,
+        public height:number,
+        public data:string
+    ) {
+        this.hash = Block.calculateHash(prevHash, height, data);
+    }
+    static calculateHash(prevHash:string, height:number, data:string){
+        const toHash = `${prevHash}${height}${data}`;
+
+    }
+}   
+
